6fa9fa48602bca3fb967582befcb0feb0fb30070
sam0402 <sam0402@gmail.com>
2021-01-23 14:37:48 +0800

    Create install-roonbridge_v8.sh

---
+++ install-roonbridge_v8.sh
@@ -0,0 +1,27 @@
+#!/bin/busybox ash
+
+. /etc/init.d/tc-functions
+. /var/www/cgi-bin/pcp-functions
+
+useBusybox
+TARGET=`cat /etc/sysconfig/backup_device`
+
+
+sudo -u tc tce-load -wi pcp-ffmpeg.tcz
+cd /opt
+wget http://download.roonlabs.com/updates/stable/RoonBridge_linuxarmv8.tar.bz2
+tar xf RoonBridge_linuxarmv8.tar.bz2
+rm -f /opt/RoonBridge_linuxarmv8.tar.bz2
+
+mkdir -p /.RoonBridge/Settings
+echo AskAlways > /.RoonBridge/Settings/update_mode
+mkdir /.RAATServer
+
+pcp_write_var_to_config USER_COMMAND_1 "%2fopt%2fRoonBridge%2fstart.sh"
+
+sed -i '/.RoonBridge/d' /opt/.filetool.lst
+sed -i '/.RAATServer/d' /opt/.filetool.lst
+echo '.RoonBridge' >>/opt/.filetool.lst
+echo '.RAATServer' >>/opt/.filetool.lst
+
+filetool.sh -b
